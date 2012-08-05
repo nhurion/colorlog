dd34669c0b4a2d6c4d89ac48bfd1cf149c63c2b1
Nicolas Hurion <nicolas@hurion.eu>
2012-08-05 22:52:09 +0200

    Add option to chose on what color is set, and add documentation

--- tl.rb
+++ tl.rb
@@ -1,13 +1,46 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
 require 'rainbow'
+
+options = {}
+option_parser = OptionParser.new do |opts|
+  executable_name = File.basename($PROGRAM_NAME)
+  opts.banner = "Read a file and color the lines in " +
+      "green".foreground(:green) + ", " +
+      "yellow".foreground(:yellow) + " or " +
+      "red".foreground(:red) + " based on the content of the line
+
+Usage: #{executable_name} [options] filename
+
+"
+
+  options[:green] = "success"
+  opts.on('-g RegExp','RegExp that matched will turn the line green') do |green|
+    options[:green] = green
+  end
+
+  options[:yellow] = "warning"
+  opts.on('-y RegExp','RegExp that matched will turn the line yellow') do |yellow|
+    options[:yellow] = yellow
+  end
+
+  options[:red] = "failure|error"
+  opts.on('-r RedExp','RegExp that matched will turn the line red') do |red|
+    options[:red] = red
+  end
+
+end
+
+option_parser.parse!
+
+success_check = Regexp.new(options[:green], Regexp::IGNORECASE)
+warning_check = Regexp.new(options[:yellow], Regexp::IGNORECASE)
+error_check = Regexp.new(options[:red], Regexp::IGNORECASE)
 
 ARGF.each do |line|
 
   color = :default
-  success_check = Regexp.new("success", Regexp::IGNORECASE)
-  warning_check = Regexp.new("warning", Regexp::IGNORECASE)
-  error_check = Regexp.new("failure|error", Regexp::IGNORECASE)
 
   if line =~ success_check
     color = :green
